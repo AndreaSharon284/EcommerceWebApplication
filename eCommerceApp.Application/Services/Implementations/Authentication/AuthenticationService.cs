ab890c22656360758f69d9da0de8dd81b5e07aaa
Andrea Sharon Silva <sharonandrea245@gmail.com>
2025-06-30 03:19:25 +0530

    Implement Authentication Service for application login

--- eCommerceApp.Application/Services/Implementations/Authentication/AuthenticationService.cs
+++ eCommerceApp.Application/Services/Implementations/Authentication/AuthenticationService.cs
@@ -1,10 +1,13 @@
 using eCommerceApp.Application.DTOs;
 using eCommerceApp.Application.DTOs.Identity;
 using eCommerceApp.Application.Services.Interfaces.Authentication;
+using eCommerceApp.Application.Services.Interfaces.Logging;
+using eCommerceApp.Domain.Interfaces.Authentication;
 
 namespace eCommerceApp.Application.Services.Implementations.Authentication;
 
-public class AuthenticationService : IAuthenticationService
+public class AuthenticationService(ITokenManagement tokenManagement, IUserManagement userManagement,
+    IRoleManagement roleManagement, IApplogger<AuthenticationService> logger) : IAuthenticationService
 {
     public Task<ServiceResponse> CreateUser(CreateUser user)
     {
